3b7a9c812227fd2270d39de6b8efd208f0dc399c
arpadpall21 <a.pall21@yahoo.fr>
2025-02-15 17:11:06 +0100

    got all necessary tools to build

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,5 @@
 import fs from 'node:fs';
-import readline from 'node:readline';
+import * as readline from 'node:readline';
 import randomQuestionPicker from './misc/randomQestionPicker.mjs';
 import config from './config.mjs';
 import color from './misc/color.mjs';
@@ -9,40 +9,49 @@ const { welcome, roules, passMessage, failMessage } = JSON.parse(fs.readFileSync
 const readLineInterface = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  terminal: true,
 });
 
-// process.stdin.setRawMode(true);
-// process.stdin.resume();
+readLineInterface.output.write('\x1B[?25l');
 
-// process.stdin.on('data', (key) => {
-//   console.log('stdin key pressed')
-//   console.log(key)
-// })
-
-process.stdin.on('keypress', (key) => {
-  console.log('stout key pressed')
-  console.log(key)
-})
-
-process.stdin.on('data', (data) => {
-  console.log('stout data')
-  console.log(data.toString())
+process.stdout.on('data', (data) => {
+  const key = data.toString()
+  console.log(data)
+  
+  if (data.toString() === 'q') {
+    // readLineInterface.close();
+    process.exit()
+  }
 })
 
 
-console.log(config.minCorrectAnswers)
+
+// readline.cursorTo(readLineInterface.output, 0);
+//     // Clear the current line
+//     readline.clearLine(readLineInterface.output, 0);
+
+// console.log(config.minCorrectAnswers)
 
 
 const startTestYes = `  ${color.green}yes ◄${color.reset}\n  no`;
 const startTestNo = '  yes\n  no◄';
 
 readLineInterface.write(`${welcome}\n`);
-readLineInterface.write(`${roules}\n\n`);
+readLineInterface.write(`${roules}\n\nPress 'q' to exit the program\n\n`);
+
+
 
 const requiredCorrectAnswers = config.displayCurrentSuccessRate
-  ? `(Required correct answers: ${config.minCorrectAnswers})\n`
+  ? `Required correct answers: ${config.minCorrectAnswers}\n`
   : '\n';
-readLineInterface.write(`Total questions: ${config.numberOfQuestions} ${requiredCorrectAnswers}`);
+readLineInterface.write(`Total questions: ${config.numberOfQuestions}\n${requiredCorrectAnswers}`);
+
+
+
 
 readLineInterface.write(`Are you ready to start?\n${startTestYes}`);
+
+
+readline.cursorTo(readLineInterface.output, 0);
+readline.moveCursor(readLineInterface.output, 0, -1);
+
+readline.clearScreenDown(readLineInterface.output);
